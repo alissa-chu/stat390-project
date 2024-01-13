9297f98a59febd440b7272291160ecee82350b45
Ryan <103151546+ryannguyen275@users.noreply.github.com>
2024-01-13 13:45:13 -0600

    Ryan: adding abbreviations

---
+++ 00_initial_EDA.R
@@ -0,0 +1,32 @@
+# 00 initial exploratory data analysis
+library(tidymodels)
+library(tidyverse)
+library(kableExtra)
+library(corrplot)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# seed
+set.seed(3013)
+
+vaccines <- read_csv("data/COVID-19_Vaccinations_in_the_United_States_Jurisdiction_20240111.csv")
+deaths <- read_csv("data/Provisional_COVID-19_Death_Counts_by_Week_Ending_Date_and_State_20240113.csv")
+
+deaths <- deaths %>% 
+  mutate(Location = state.abb[match(State,state.name)])
+
+write_csv(deaths, file = "data/deaths_with_abbs.csv")
+
+skimr::skim(covid)
+
+# looking at missing data
+missing_table <- naniar::miss_var_summary(covid) %>% 
+  # filter(pct_miss > 20) %>% 
+  kbl() %>% 
+  kable_classic()
+
+missing_table
+
+covid %>% 
+  select(Second_Booster)
